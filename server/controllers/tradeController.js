b43d291a13dfc0f6034457c63256bdd823985ead
fayiztv <tvfayiztv@gmail.com>
2024-01-30 17:06:33 +0530

    controllers completed

--- server/controllers/tradeController.js
+++ server/controllers/tradeController.js
@@ -21,4 +21,29 @@ export async function createTrade(req, res) {
   }
 }
 
+export async function getAllTrades(req, res) {
+  try {
+    const { type, user_id } = req.query;
+    let query = {};
+    if (type) query.type = type;
+    if (user_id) query.user_id = user_id;
+    const trades = await TradeModel.find(query).sort("id");
+    res.status(200).json(trades);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
 
+export async function getTradeById(req, res) {
+  try {
+    const trade = await TradeModel.findOne({ id: req.params.id });
+    if (!trade) return res.status(404).send("ID not found");
+    res.status(200).json(trade);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
+export async function handleNotAllowed(req, res) {
+  res.status(405).send("Method Not Allowed");
+}
--- server/routers/tradeRoutes.js
+++ server/routers/tradeRoutes.js
@@ -1,8 +1,10 @@
 import express from "express" 
-import { createTrade } from "../controllers/tradeController.js";
+import { createTrade, getAllTrades, getTradeById, handleNotAllowed } from "../controllers/tradeController.js";
 const router = express.Router();
 
 router.post('/trades', createTrade);
-
+router.get('/trades', getAllTrades);
+router.get('/trades/:id', getTradeById);
+router.all('/trades/:id', handleNotAllowed);
 
 export default router;
